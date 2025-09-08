7f47693be2a53a7c0aae224120df4907fbc61516
Joe <joebuildscode@gmail.com>
2025-09-08 10:02:46 -0500

    Finished Day 2 for 100 Days of SwiftUI, which included Booleans and String Interpolation and the first Checkpoint

--- foundations/Day2.playground/Contents.swift
+++ foundations/Day2.playground/Contents.swift
@@ -1,6 +1,32 @@
 import Cocoa
 
 // Day 2: Booleans
+let filename = "paris.jpg"
+print(filename.hasSuffix(".jpg")) // <-- True
 
+let number = 120
+print(number.isMultiple(of: 3)) // <-- True
+
+let goodDogs = true
+var gameOver = false
+gameOver.toggle() // <-- Changes to true
+
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated // <-- Flip betweeen true and false can also use .toggle()
+print(isAuthenticated)
 
 // Day 2: String Interpolation
+let firstPart = "Hello, "
+let secondPart = "world!"
+let greeting = firstPart + secondPart // <-- "Hello, world!"
+
+let name = "Taylor"
+let age = 26
+let message = "Hello, my name is \(name) and I'm \(age) years old."
+print(message)
+
+// Checkpoint 1: Celsius to Fahrenheit Conversion
+let temperatureInCelsius = 32.5
+let temperatureInFahrenheit = temperatureInCelsius * 9 / 5 + 32
+let temperatures = "It is \(temperatureInCelsius) degrees Celsius or \(temperatureInFahrenheit) degrees Fahrenheit."
+print(temperatures)
